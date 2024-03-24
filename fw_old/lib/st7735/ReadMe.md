d9269171736d9954c6b2a6a49ba25236d9278c4a
Jay <33371449+jay94ks@users.noreply.github.com>
2024-03-24 22:30:59 +0900

    Old firmware codes deletion.

--- fw_old/lib/st7735/ReadMe.md
+++
@@ -1,2 +0,0 @@
-## This is ST7735 library.
-https://github.com/gavinlyonsrepo/ST7735_TFT_PICO
--- fw_old/usb/usbd_uid.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdint.h>
-#include <pico/unique_id.h>
-#include "usbd.h"
-
-void usbd_get_uid(char* buf, uint32_t len) {
-    pico_get_unique_board_id_string(buf, len);
-}
